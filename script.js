bc487b9d7d7bda4357e0ac0f2f92f22269030b9f
Noni86kg <nonisc2@gmail.com>
2021-10-08 21:13:32 +0200

    Zadatak 5

--- script.js
+++ script.js
@@ -1,18 +1,111 @@
 const toggleBtn = document.querySelector('.toggle');
 const togleCircle = document.querySelector('.togle-circle');
+const buttons = document.querySelectorAll('button');
+const displayNum = document.querySelector('.display-num');
+let currentTheme = null;
+let prevNum = null;
+let currentNum = null;
+let isSecondNumber = false;
+let prevOperator = null;
 
+function pressNumber(number) {
+    let displayContent = displayNum.textContent;
 
+    if (isSecondNumber) {
+        displayContent = '';
+        isSecondNumber = false;
+    }
 
+    displayNum.textContent = displayContent === '0' ? number : displayContent + number;
+}
 
+function addDecimal() {
+    !displayNum.textContent.includes('.') ? displayNum.textContent = `${displayNum.textContent}.` : displayNum.textContent;
+}
 
+function pressOperator(operator) {
+    currentNum = Number(displayNum.textContent);
 
+    if (!prevNum) {
+        prevNum = currentNum;
+        prevOperator = operator;
+    }
+    else {
+        switch (prevOperator) {
+            case '+':
+                currentNum = prevNum + currentNum;
+                displayNum.textContent = currentNum;
+                console.log(currentNum)
+                console.log(operator)
+                break;
+            case '-':
+                currentNum = prevNum - currentNum
+                displayNum.textContent = currentNum;
+                break;
+            case '*':
+                currentNum = prevNum * currentNum
+                displayNum.textContent = currentNum;
+                break;
+            case '/':
+                currentNum = prevNum / currentNum
+                displayNum.textContent = currentNum;
+                break;
+            case '=':
+                displayNum.textContent = currentNum;
+            default:
+                displayNum.textContent = prevNum;
+                break;
+        }
+        prevOperator = operator;
+        prevNum = currentNum;
+    }
 
+    isSecondNumber = true;
+}
 
+function deleteElement() {
+    displayNum.textContent.length === 1 ? displayNum.textContent = 0 : displayNum.textContent = displayNum.textContent.slice(0, -1);
+}
 
+function resetCalculator() {
+    prevNum = null;
+    currentNum = null;
+    operator = null;
+    isSecondNumber = false
+    displayNum.textContent = '';
+}
 
+buttons.forEach((button) => {
+    if (button.classList.contains('number')) {
+        button.addEventListener('click', function(e) {
+            pressNumber(button.value);
+        })
+    }
+    if (button.classList.contains('decimal')) {
+        button.addEventListener('click', function(e) {
+        addDecimal();
+        })
+    }
 
+    if (button.classList.contains('operator')) {
+        button.addEventListener('click', function(e) {
+            pressOperator(button.value);
+        })
+    }
 
+    if (button.classList.contains('delete')) {
+        button.addEventListener('click', function(e) {
+            deleteElement();
+        })
+    }
 
+    if (button.classList.contains('reset')) {
+        button.addEventListener('click', function(e) {
+            resetCalculator();
+        })
+    }
+
+})
 
 
 // 3 modes
@@ -80,4 +173,3 @@ toggleBtn.addEventListener('click' , function(e) {
         document.documentElement.style.setProperty('--red1toggleBgHover', 'rgb(249, 107, 91)');
     }
 })
-
